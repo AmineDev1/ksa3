ba8611a197d4694d5387c0d7b639e3a903aa32ee
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2019-01-06 22:13:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2499,4 +2499,22 @@ client.on("guildMemberAdd", member => {
 انت العضو رقم ${member.guild.memberCount} `) 
 }).catch(console.error)
 })
+
+
+var prefix = "-";
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
 client.login(process.env.BOT_TOKEN);
